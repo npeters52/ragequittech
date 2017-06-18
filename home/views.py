bbca4d3fd5cfbeb152999da151d581e54e7b01fc
Nick <petersnicholas2@gmail.com>
2017-06-18 02:18:10 -0500

    blog integration into homepage, working on images

--- home/views.py
+++ home/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 from polls.models import Question, Choice
+from blog.models import Article, Category
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
- 
+
 def home(request):
     latest_question_list = Question.objects.order_by('-created')[:5]
+    top_article = Article.objects.order_by('pub_date')[:1]
+    article_list = Article.objects.order_by('-pub_date')[1:5]
     context = {
-        'latest_question_list':latest_question_list
+        'latest_question_list':latest_question_list,
+        'article_list':article_list,
+        'top_article':top_article
     }
     return render(request, 'home/home.html', context)
-
-
-
