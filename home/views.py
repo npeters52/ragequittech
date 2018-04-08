4882e3a77d48c26f09625e00616ddde8e29db2c5
Nick Peters <petersnicholas2@gmail.com>
2018-04-08 11:33:06 -0700

    footer to homepage, centered homepage podcasts

--- home/views.py
+++ home/views.py
@@ -5,16 +5,19 @@ from blog.models import Article, Category
 from podcast.models import Podcast
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+import datetime
 
 def home(request):
     latest_question_list = Question.objects.order_by('-created')[:2]
     top_article = Article.objects.order_by('-pub_date')[:1]
     article_list = Article.objects.order_by('-pub_date')[1:5]
-    top_podcast = Podcast.objects.order_by('-pub_date')
+    podcast_list = Podcast.objects.order_by('-pub_date')
+    now = datetime.datetime.now()
     context = {
         'latest_question_list':latest_question_list,
         'article_list':article_list,
         'top_article':top_article,
-        'top_podcast':top_podcast
+        'podcast_list':podcast_list,
+        'now':now
     }
     return render(request, 'home/home.html', context)
