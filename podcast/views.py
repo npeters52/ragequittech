c6076db41eff7b1c018d3edf6e5acd5d9337c6d8
Nick Peters <petersnicholas2@gmail.com>
2018-07-21 19:59:38 -0700

    podcast index and detail styling

--- podcast/views.py
+++ podcast/views.py
@@ -10,7 +10,7 @@ def podcast_index(request):
     podcast_list = Podcast.objects.filter(is_published=True).order_by('-pub_date')[:3]
     podcast_archive = Podcast.objects.filter(is_published=True).order_by('-pub_date')
     context = {
-        'podcast_list':podcast_list
+        'podcast_list':podcast_list,
         'podcast_archive':podcast_archive
     }
     return render(request, 'podcast/podcast_index.html', context)
