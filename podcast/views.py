39a03b389e59e50c436e482292c1b960502146a5
Nick Peters <petersnicholas2@gmail.com>
2018-07-21 22:01:22 -0700

    podcast views troubleshooting

--- podcast/views.py
+++ podcast/views.py
@@ -7,7 +7,7 @@ from .models import Podcast, Channel
 # Create your views here.
 
 def podcast_index(request):
-    podcast_archive = Podcast.objects.filter(is_published=True).order_by('-pub_date')
+    podcast_list = Podcast.objects.filter(is_published=True).order_by('-pub_date')
     context = {
         'podcast_archive': podcast_archive
     }
