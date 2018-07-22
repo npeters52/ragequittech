137794e5e4b105a5766abe7e48c612a96d7a620a
Nick Peters <petersnicholas2@gmail.com>
2018-07-21 22:30:13 -0700

    podcast views troubleshooting

--- podcast/templatetags/podcast_extras.py
+++ podcast/templatetags/podcast_extras.py
@@ -9,7 +9,7 @@ import re
 def youtube_embed_url(value):
     match = re.search(r'^(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?$', value)
     if match:
-        embed_url = 'http://www.youtube.com/embed/%s' %(match.group(2))
+        embed_url = 'https://www.youtube.com/embed/%s' %(match.group(2))
         res = "<iframe width=\"1060\" height=\"600\" src=\"%s\" frameborder=\"0\" allowfullscreen></iframe>" %(embed_url)
         return res
     return ''
