56d9a1f7c9ced2897baa77b659e38b158573a6f2
Nick Peters <petersnicholas2@gmail.com>
2018-07-21 21:57:50 -0700

    blog title font change

--- home/views.py
+++ home/views.py
@@ -12,12 +12,14 @@ def home(request):
     top_article = Article.objects.order_by('-pub_date')[:1]
     article_list = Article.objects.order_by('-pub_date')[1:4]
     podcast_list = Podcast.objects.order_by('-pub_date')[:3]
+    podcast_archive = Podcast.objects.order_by('pub_date')
     now = datetime.datetime.now()
     context = {
         'latest_question_list':latest_question_list,
         'article_list':article_list,
         'top_article':top_article,
         'podcast_list':podcast_list,
+        'podcast_archive':podcast_archive,
         'now':now
     }
     return render(request, 'home/home.html', context)
