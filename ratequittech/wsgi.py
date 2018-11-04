5a59270ce25cbeb6c6f2059ad11b69e730c96beb
Nick Peters <petersnicholas2@gmail.com>
2018-11-03 18:48:40 -0700

    wsgi .gitignore commits

--- ratequittech/wsgi.py
+++ ratequittech/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budget.settings")
+path = '/home/ragequittech/ragequittech/'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+print sys.path
 
 application = get_wsgi_application()
