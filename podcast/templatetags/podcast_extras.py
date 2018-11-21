dc4e28a672ac5a625f6b023d1a2cc8ff5fefed05
Nick <petersnicholas2@gmail.com>
2018-11-21 01:39:57 -0600

    responsive styling

--- podcast/templatetags/podcast_extras.py
+++ podcast/templatetags/podcast_extras.py
@@ -10,7 +10,7 @@ def youtube_embed_url(value):
     match = re.search(r'^(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?$', value)
     if match:
         embed_url = 'https://www.youtube.com/embed/%s' %(match.group(2))
-        res = "<iframe width=\"1060\" height=\"600\" src=\"%s\" frameborder=\"0\" allowfullscreen></iframe>" %(embed_url)
+        res = "<iframe class="podcast-iframe" src=\"%s\" frameborder=\"0\" allowfullscreen></iframe>" %(embed_url)
         return res
     return ''
 
