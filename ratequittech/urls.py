6e74d7a53854eb7fff527ed451d668d025251021
Nick Peters <petersnicholas2@gmail.com>
2019-02-03 14:47:35 -0800

    local-dev

--- ratequittech/urls.py
+++ ratequittech/urls.py
@@ -32,3 +32,6 @@ urlpatterns = [
     url(r'^search/', search, name='search'),
     url(r'^about/', TemplateView.as_view(template_name='home/about.html'), name="about")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
